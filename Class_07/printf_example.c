439b61ec5a5daaa342b22a07ad8b8e4ff4c3bde1
salor47 <game7518@gmail.com>
2022-10-04 14:08:40 +0300

    Class_

--- Class_07/printf_example.c
+++ Class_07/printf_example.c
@@ -1,9 +1,13 @@
+
 /* printf example */
 #include <stdio.h>
 
 int main()
 {
+   printf ("Characters: %o %o \n", 'a', 65);
    printf ("Characters: %c %c \n", 'a', 65);
+   printf ("Characters: %c %c \n", 'a', 65);
+   printf ("Characters: %10d %d \n", 6,739735);
    printf ("Decimals: %d %ld\n", 1977, 650000L);
    printf ("Preceding with blanks: %10d \n", 1977);
    printf ("Preceding with zeros: %010d \n", 1977);
---
+++ Class_08/backslashes.c
@@ -0,0 +1,18 @@
+/*
+\n - parcel kursoru jauna rinda
+\t - parcel kursoru uz tuvako tabulacija poziciju
+\v - vertikali uz leju/ vertikalais tabs
+\b - parcel kursoru vienu poziciju atpakal
+\a - bip!
+\r - parcel kursoru uz rindas sakumu
+*/
+
+print("");
+print("");
+print("");
+print("");
+print("");
+print("");
+print("");
+
+
