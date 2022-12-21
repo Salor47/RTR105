2eb16e70339f80347dfccbed8a44326e0251d0f6
Salor47 <99359849+Salor47@users.noreply.github.com>
2022-12-21 15:07:18 +0200

    Create summa.c

---
+++ Uzdevumi/PU6/summa.c
@@ -0,0 +1,10 @@
+////// SUMMA //////
+#include <user_defined_function.h>
+#include <stdio.h>
+
+void summa(void){
+   int a = 2, b = 2, c;
+   c = a + b;
+   fprintf(stderr, "c ir summas vertiba = %d\n", c);
+   //nevajag return
+}
