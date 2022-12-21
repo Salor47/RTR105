31abfc4e26ff9bebfe682a20ba0c43ffe1430762
Salor47 <99359849+Salor47@users.noreply.github.com>
2022-12-21 15:09:04 +0200

    Create dalisana.c

---
+++ Uzdevumi/PU6/dalisana.c
@@ -0,0 +1,9 @@
+////// DALISANA //////
+#include <user_defined_function.h>
+#include <stdio.h>
+
+int dalisana(int k, int l){
+   int j;
+   j = k / l;
+   return j; //atdod j jeb dalisanas rezultatu
+}
