ba90d4ce6e45f0d492e66d1d41b66328668d924b
Salor47 <99359849+Salor47@users.noreply.github.com>
2023-01-06 09:20:06 +0200

    Update integral.c

--- Laboratorijas_darbi/LD4_Integral/integral.c
+++ Laboratorijas_darbi/LD4_Integral/integral.c
@@ -20,13 +20,13 @@ int main() {
    printf("\nIevadi darbības precizitāti = ");
    scanf("%e", &x);
 
-  integr2=(b-a)*(sin(a/2)+sin(b/2))/i;
+  integr2=(b-a)*(cos(a/2)+cos(b/2))/i;
   while(fabs(integr2-integr1)>x){
       i*=2;
       h=(b-a)/i;
       integr1=integr2;
       integr2=0.;
-        for(j=0;j<i;j++)integr2+=h*sin((a+(j+0,5)*h)/2); 
+        for(j=0;j<i;j++)integr2+=h*cos((a+(j+0,5)*h)/2); 
   }
     printf("Integrala vertiba: %.2f\n", integr2);
     return 0;
